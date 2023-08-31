c613ea6871a4fa7c0851ff52ea91876ae50fcbea
Hossam <56669856+HOSS11H@users.noreply.github.com>
2023-08-31 03:17:40 +0300

    init

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+
+export default async function middleware(req) {
+
+}
+
+export const config = {
+  matcher: [],
+}
---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import { withContentlayer } from "next-contentlayer"
+
+import "./env.mjs"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+}
+
+export default withContentlayer(nextConfig)
